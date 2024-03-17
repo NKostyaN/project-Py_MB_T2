e478cd3aae1e0b9497f68800af81c0d8a2535561
ViacheslavKuznetsov <155986232+samatwa@users.noreply.github.com>
2024-03-17 10:43:09 +0100

    changes

--- MontyBot/modules/notebook.py
+++ MontyBot/modules/notebook.py
@@ -3,44 +3,34 @@ from modules.note import Note
 
 class NoteBook:
     def __init__(self):
-        self.notes = []
+        self.notes: list[Note] = []
 
-    def add_note(self, title: str, text: str, tags:list[str]):     # it was changed # type: ignore
-        self.notes.append(Note(title, text, tags))
+    def add_note(self, note: Note):
+        self.notes.append(note)
 
     def edit_note(self, title: str, new_text: str):
-        for i in self.notes:
-            if i.title == title:
-                i.text = new_text
+        for n in self.notes:
+            if n.title == title:
+                n.text = new_text
 
     def remove_note(self, title: str):
-        for i in self.notes:
-            if i.title == title:
-                self.notes.remove(i)
+        self.notes = [n for n in self.notes if n.title != title]
 
     def find_by_title(self, title: str) -> Note:
-        for note in self.notes:
-            if title in note.title:
-                return note
-            
-    def search_notes_by_tag(self, tag):
-        found_notes = []
-        for note in self.notes:
-            if tag in note.tags:
-                found_notes.append(note)
-        return found_notes
+        for n in self.notes:
+            if n.title == title:
+                return n
 
-    def sort_notes_by_tag(self, tag):
-        sorted_notes = sorted(self.notes, key=lambda note: tag in note.tags)
-        return sorted_notes
+    def search_notes_by_tag(self, tag: str) -> list[Note]:
+        return [n for n in self.notes if tag in n.tags]
 
+    def sort_notes_by_tag(self, tag: str) -> list[Note]:
+        return sorted(self.notes, key=lambda n: tag in n.tags)
 
-    # it was changed
     def to_json(self) -> dict:
         res = {}
-        for item in self.notes:
-            res[item.title] = {
-                "text": item.text,
-                "tags": item.tags  # Assuming 'tags' is an attribute of the note item
+        for n in self.notes:
+            res[n.title] = {
+                "text": n.text, "tags": n.tags
             }
         return res
